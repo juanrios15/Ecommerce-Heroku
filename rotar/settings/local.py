e4081b4db88a691faf3d21f320198e4b51cfb0d3
Juan Rios <juankrios15@gmail.com>
2021-04-21 17:13:19 -0500

    sdaf

--- rotar/settings/local.py
+++ rotar/settings/local.py
@@ -9,6 +9,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['morning-savannah-31438.herokuapp.com']
 
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
