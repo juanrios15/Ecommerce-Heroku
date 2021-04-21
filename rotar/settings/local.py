f870bbac9f512ada2177dc10dbb81bd5f6360349
Juan Rios <juankrios15@gmail.com>
2021-04-21 17:06:34 -0500

    asd

--- rotar/settings/local.py
+++ rotar/settings/local.py
@@ -2,10 +2,7 @@ import dj_database_url
 from .base import *
 import os
 import psycopg2
-
-DATABASE_URL = os.environ['DATABASE_URL']
-
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+from decouple import config
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -16,17 +13,10 @@ ALLOWED_HOSTS = ['morning-savannah-31438.herokuapp.com']
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': "dbecommerce",
-        'USER': "juanrios",
-        'PASSWORD': "z3r4tul89",
-        'HOST': 'localhost',
-        'PORT': '5432'
-        
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
